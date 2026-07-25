2cfe80b38f445e7e2bd8a6ae03c5a9b4dd7a9674
bablu8866 <bablukumar8866bk@gmail.com>
2026-07-25 20:00:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validateForm() {
     var email = document.getElementById("email").value;
     var msg = document.getElementById("message").value;
     var result = document.getElementById("result");
-
+    
     if (name === "" || email === "" || msg === "") {
         result.innerHTML = "❌ सभी fields भरें / Fill all fields";
         result.style.color = "red";
@@ -11,7 +11,7 @@ function validateForm() {
         result.innerHTML = "❌ Valid email दर्ज करें";
         result.style.color = "red";
     } else {
-        result.innerHTML = "✔ फ़ॉर्म सफलतापूर्वक जमा हो गया। धन्यवाद।";
+        result.innerHTML = "✅ Form submitted successfully!";
         result.style.color = "green";
     }
 }
