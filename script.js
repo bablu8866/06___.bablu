d751bf199ccd700019e08ca255f54a86bba505fc
bablu8866 <bablukumar8866bk@gmail.com>
2026-07-26 00:23:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,44 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+// Typing Effect
+
+const text = [
+  "Frontend Web Developer",
+  "HTML | CSS | JavaScript",
+  "Web Designer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+  if (count === text.length) {
+    count = 0;
+  }
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".hero-text h3").textContent = letter;
+
+  if (letter.length === currentText.length) {
+
+    setTimeout(() => {
+
+      index = 0;
+      count++;
+      type();
+
+    }, 1500);
+
+  } else {
+
+    setTimeout(type, 100);
+
+  }
+
+})();
