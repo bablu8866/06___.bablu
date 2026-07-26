5ffef02d39ba4adc1ab81c4758e8267078f7b02d
bablu8866 <bablukumar8866bk@gmail.com>
2026-07-26 17:15:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -123,3 +123,25 @@ lightbox.addEventListener("click", (e) => {
         lightbox.style.display = "none";
     }
 });
+// Gallery Lightbox
+const galleryImages = document.querySelectorAll(".gallery-box img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+galleryImages.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+    }
+});
