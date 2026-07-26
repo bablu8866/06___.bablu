fcc2497a0a0403779e6b8d62c73c200244995742
bablu8866 <bablukumar8866bk@gmail.com>
2026-07-26 22:45:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -124,3 +124,26 @@ lightbox.addEventListener("click", (e) => {
         lightbox.style.display = "none";
     }
 });
+// Gallery Lightbox
+
+const images=document.querySelectorAll(".gallery-item img");
+const lightbox=document.getElementById("lightbox");
+const lightboxImg=document.getElementById("lightbox-img");
+const close=document.querySelector(".close");
+
+images.forEach(img=>{
+img.onclick=()=>{
+lightbox.style.display="flex";
+lightboxImg.src=img.src;
+}
+});
+
+close.onclick=()=>{
+lightbox.style.display="none";
+}
+
+lightbox.onclick=(e)=>{
+if(e.target===lightbox){
+lightbox.style.display="none";
+}
+}
